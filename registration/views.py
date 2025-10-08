f8f9d3ee103eaccf703f6943192185682b29ebb3
AndreDizon <cardizon.student@ua.edu.ph>
2025-10-08 09:21:01 +0800

    first commit

---
+++ registration/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import UserRegistration
+from .serializer import UserRegistrationSerializer
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['POST'])
+def register_user(request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
